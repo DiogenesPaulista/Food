c07fed44a4aed5e6023480a54d569783072e0987
Diogenes Rosa Paulista <diogenesros@hotmail.com>
2024-01-24 11:52:46 -0300

    Exemplo de aplicação em react

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,22 @@
 import Navbar from "../../components/navbar/navbar.jsx";
+import ProdutoVitrine from "../../components/produto-vitrine/produto-vitrine.jsx";
 
 function Home(){
     return <>
         <Navbar />
-        Nosso Cardápio<br/>
-        Clique em adicionar para colocar os produtos na sacola de compra
-        Se preferir, você pode pedir pelo WhatsApp: (11) 94785-2140 <br/>
-        Lista dos produtos...   
+
+        <div className="container">
+            <div className="titulo text-center">
+                <h1>Nosso Cardápio</h1>
+                <p className="subtitulo">Clique em adicionar para colocar os produtos na sacola de compra. Se preferir, você pode pedir pelo WhatsApp: (11) 94785-2140</p>
+            </div>
+        </div>
+
+        <ProdutoVitrine />
+        <ProdutoVitrine />
+        <ProdutoVitrine />
+        <ProdutoVitrine />
+
     </>
 }
 
