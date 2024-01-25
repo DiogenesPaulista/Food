a898d272f00dd13e210474820b741fe9e460764a
Diogenes Rosa Paulista <diogenesros@hotmail.com>
2024-01-25 14:57:27 -0300

    Exemplo de aplicação em react

--- src/pages/historico/historico.jsx
+++ src/pages/historico/historico.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../../components/navbar/navbar.jsx";
 import "./historico.css";
+import {pedidos} from"../../dados.js";
 
 function Historico(){
 
@@ -22,32 +23,16 @@ function Historico(){
             </div>
             <div className="box-pedido">
                 <table className="table">
-                    <tr>
-                        <td> <strong>Pedido 321321</strong></td>
-                        <td className="text-light"> 14/01/2024</td>
-                        <td className="text-red"> R$ 500,00</td>
-                    </tr>
-                    <tr>
-                        <td> <strong>Pedido 321321</strong></td>
-                        <td className="text-light"> 14/01/2024</td>
-                        <td className="text-red"> R$ 500,00</td>
-                    </tr>
-                    <tr>
-                        <td> <strong>Pedido 321321</strong></td>
-                        <td className="text-light"> 14/01/2024</td>
-                        <td className="text-red"> R$ 500,00</td>
-                    </tr>
-                    <tr>
-                        <td> <strong>Pedido 321321</strong></td>
-                        <td className="text-light"> 14/01/2024</td>
-                        <td className="text-red"> R$ 500,00</td>
-                    </tr>
-                    <tr>
-                        <td> <strong>Pedido 321321</strong></td>
-                        <td className="text-light"> 14/01/2024</td>
-                        <td className="text-red"> R$ 500,00</td>
-                    </tr>
-                  
+                    {
+                        pedidos.map( function(ped){
+                            return <tr key={ped.id_pedido}>
+                                <td> <strong>Pedido {ped.id_pedido}</strong></td>
+                                <td className="text-light"> {ped.dt}</td>
+                                <td className="text-red">{new Intl.NumberFormat('pt-BR', {style: 'currency', currency: "BRL"}).format(ped.total)}</td>
+                                </tr>
+                        })
+                   
+                    }
                 </table>
             </div>
         </div>
